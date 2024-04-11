2c42ab4226e6f8f14af46955697778084bd83894
kriyip <kyip719@gmail.com>
2024-04-11 15:30:37 -0400

    Initial Commit

---
+++ Runger/RungerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RungerApp.swift
+//  Runger
+//
+//  Created by Kristine Yip on 4/11/24.
+//
+
+import SwiftUI
+
+@main
+struct RungerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
